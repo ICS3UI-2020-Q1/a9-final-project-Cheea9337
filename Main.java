bcb0f9ab84c9c744060aa5d095d9dff4033ae127
Cheea9337 <cheea9337@wrdsb.ca>
2020-11-04 13:05:31 +0000

    started final project

--- Main.java
+++ Main.java
@@ -5,8 +5,15 @@ import java.awt.event.*;
 public class Main implements Runnable, ActionListener{
 
   // Class Variables  
-  
 
+  // create an array for the creation of the buttons
+  JButton[] spots;
+
+  // create an array for the cards
+  String[] cards;
+
+  // create a label to give the user instructions
+  JLabel instructions;
 
   // Method to assemble our GUI
   public void run(){
@@ -19,7 +26,65 @@ public class Main implements Runnable, ActionListener{
     // shows the window
     frame.setVisible(true);
  
-    
+    // create a main panel to have a grid of buttons
+    JPanel mainPanel = new JPanel();
+    mainPanel.setLayout(new BorderLayout());
+
+    // create and add instruction text
+    instructions = new JLabel();
+    mainPanel.add(instructions, BorderLayout.PAGE_START);
+
+    // create the panel to layout the buttons 
+    JPanel grid = new JPanel();
+    grid.setLayout(new GridLayout(4,3));
+
+    // create an array with 12 spots for the buttons 
+    spots = new JButton[12];
+
+    // create a for loop to create the buttons and add them to the panel 
+    for(int i = 0; i < spots.length; i++){
+      // create buttons with no text 
+      spots[i] = new JButton("");
+      // add action listener 
+      spots[i].addActionListener(this);
+      // set action command to the number
+      String spotNum = String.valueOf(i);
+      spots[i].setActionCommand(spotNum);
+
+      // create the fisher yates shuffle
+      function shuffle(array) {
+      var copy = [], n = spots.length, i;
+
+      // While there remain elements to shuffle…
+      while (n) {
+
+      // Pick a remaining element…
+      i = Math.floor(Math.random() * array.length);
+
+      // If not already shuffled, move it to the new array.
+      if (i in array) {
+      copy.push(array[i]);
+      delete array[i];
+      n--;
+    }
+  }
+
+    return copy;
+}
+
+      // add to the panel 
+      grid.add(spots[i]);
+    }
+
+    // add button grid to panel
+    mainPanel.add(grid, BorderLayout.CENTER);
+
+    // add panel to frame
+    frame.add(mainPanel);
+
+    cards = new String[12];
+
+
 
   }
 
